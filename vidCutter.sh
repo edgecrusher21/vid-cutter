a2ffa4d35b5a3e78909f54b68d069c0e74bdf16f
Christopher Totani <edgecrusher19@gmail.com>
2019-01-31 17:53:23 +1030

    Added a space for an if statement because bash... durrr

--- vidCutter.sh
+++ vidCutter.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+INFILE=$1
 OUTFILE="out.mp3"
 ##Example ffmpeg command
-if [ -f $OUTFILE]; then
+if [ -f $OUTFILE ]; then
    rm -rf out.mp3
    echo "out.mp3 found and has been deleted"
 fi
-youtube-dl -f251 $1 -o - | ffmpeg -i pipe: -strict -2 out.mp3
+youtube-dl -f251 $INFILE -o - | ffmpeg -i pipe: -strict -2 $OUTFILE
